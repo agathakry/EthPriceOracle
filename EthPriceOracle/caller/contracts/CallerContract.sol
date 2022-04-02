f0c823226b553a93163c7351965fdce211cd4e79
agathakry <agathakrysti@gmail.com>
2022-04-02 15:51:50 +0100

    setting up ethprice

--- EthPriceOracle/caller/contracts/CallerContract.sol
+++ EthPriceOracle/caller/contracts/CallerContract.sol
@@ -1,8 +1,21 @@
-// 1. caller/CallerContract.sol
-// declare address named oracleaddress and make it private 
-address oracleAddress private; 
-// create function takes oracle instance address 
-function setOracleInstanceAddress (address _oracleInstanceAddress) public {
-// set oracle address 
-oracleAddress = _oracleInstanceAddress
+pragma solidity 0.5.0;
+// declare and import interface for calling on other contract 
+import "./EthPriceOracleInterface.sol";
+// import Ownable contract
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+// make contract ownable
+contract CallerContract is Ownable {
+    // declare Ethprive variable 
+    EthPriceOracleInterface private oracleInstance;
+
+    // declare address named oracleaddress and make it private 
+    address private oracleAddress; 
+    // create function takes oracle instance address and attach modifier
+    function setOracleInstanceAddress (address _oracleInstanceAddress) public onlyOwner {
+    // set oracle address 
+    oracleAddress = _oracleInstanceAddress;
+    // instantiate EthPriceOracle and sore in oracleInstance 
+    oracleInstance = EthPriceOracleInterface(oracleAddress);
+    }
 }

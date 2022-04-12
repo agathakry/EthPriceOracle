1aecf57cfa3c03007f579a9ccdfef07b78ce48dd
agathakry <agathakrysti@gmail.com>
2022-04-12 20:50:02 +0100

    final part

--- EthPriceOracle/oracle/contracts/EthPriceOracle.sol
+++ EthPriceOracle/oracle/contracts/EthPriceOracle.sol
@@ -23,9 +23,72 @@ contract EthPriceOracle {
     // generate random number for the request id (avoid collusions of oracles)
     uint private randNonce = 0;
     uint private modulus = 1000;
+
+    // initialise number of oracles (to keep track)
+    uint private numOracles = 0; 
+
     mapping(uint256=>bool) pendingRequests;
+
+    // define struct response 
+    struct Response {
+        address oracleAddress;
+        address callerAddress;
+        uint256 ethPrice;
+        }
+    // mapping that defines requestIdto response
+    mapping (uint256=>Response[]) public requestIdToResponse;
+
     event GetLatestEthPriceEvent(address callerAddress, uint id);
     event SetLatestEthPriceEvent(uint256 ethPrice, address callerAddress);
+
+    // Lesson 16
+    // declare an event addoracleevent 
+    event AddOracleEvent(address oracleAddress);
+
+    // declare event that removes oracles
+    event RemoveOracleEvent(address oracleAddress);
+
+    // define constructor for ethPriceOracle for defining roles and ownership
+    constructor (address _owner) public {
+        // add _owner to the list of owners 
+        owners.add(_owner);
+    }
+
+    // define function addOracle to set up new oracle
+    function addOracle(address _oracle) public {
+        // add a requre statement to make sure owners contain msg.sender 
+        require(owners.has (msg.sender), "Not an owner");
+
+        // add a require to make sure _oracle not already an oracle
+        require(!oracles.has(_oracle), "Already an oracle!");
+
+        // call oracle.add function 
+        oracles.add(_oracle);
+
+        // increment number of oracles
+        numOracles++;
+
+        // fire an event to add oracle
+        emit AddOracleEvent(_oracle);
+    }
+
+    // define function that removes oracle and keeps track 
+    function removeOracle (address _oracle) public {
+        require(owners.has(msg.sender), "Not an owner!");
+        require(oracles.has(_oracle), "Not an oracle!");
+        // 3. Continue here
+        // add require stamement numoracles >1 
+        require(numOracles>1, "Do not remove the last oracle!");
+
+        // call remove function 
+        oracles.remove(_oracle);
+
+        // decrement oraclenum
+        numOracles--;
+
+        // fire event 
+        emit RemoveOracleEvent(_oracle);
+  }
     
     // define function that returns ethprice
     function getLatestEthPrice() public returns(uint256) {
@@ -45,9 +108,24 @@ contract EthPriceOracle {
         return id;
     }
     // create function setLatestEthpruce to retrieve from binance API
-    function setLatestEthPrice(uint256 _ethPrice, address _callerAddress, uint256 _id) public onlyOwner {
+    // remove onlyOwner for Role instead
+    //function setLatestEthPrice(uint256 _ethPrice, address _callerAddress, uint256 _id) public onlyOwner {
+    function setLatestEthPrice(uint256 _ethPrice, address _callerAddress, uint256 _id) public {
+
+        // require to make sure oracles has msg.sender 
+        require(oracles.has(msg.sender), "Not an oracle!");
+    
         // require to check pending request is true 
         require(pendingRequests[_id], "This request is not in my pending list.");
+
+        // declare response variable resp stored in memory
+        Response memory resp;
+
+        // initialise the struct
+        resp = Response(msg.sender, _callerAddress, _ethPrice);
+
+        // push resp to array stored in requestIdToResponse[_id]
+        requestIdToResponse[_id].push(resp);
         
         // remove id from the pendingRequests mapping 
         delete pendingRequests[_id];
